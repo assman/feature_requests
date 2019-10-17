8f9a864e26dc7261b8f5ff01c9f85019d8a57b8d
Varun <parmar@u.nus.edu>
2019-10-17 18:36:08 +0800

    Fixed a mistake in server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/pdpa-consent'));
+app.use(express.static(__dirname + '/dist/feature_requests'));
 
 // Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/pdpa-consent/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/feature_requests/index.html'));
 });
 
 // default Heroku PORT
